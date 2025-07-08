e6b5cba5346dec4b02065318dac7cf30a68a5e20
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-07-09 01:28:15 +0530

    COMPONENTMAKING
    - Built a standalone Button component to demonstrate basic React component structure.
    - Imported and used the Button component in App.jsx for testing and UI integration.
    - Practiced props, JSX, and component-based architecture.

---
+++ 2 react learning/src/App.jsx
@@ -0,0 +1,10 @@
+import Mybutton from "./mybutton";  
+function App(){
+return <div>
+<h1>
+   hello sarvesh 
+</h1>
+ <Mybutton></Mybutton>
+</div>
+}
+export default App ;
---
+++ 2 react learning/src/Mybutton.jsx
@@ -0,0 +1,4 @@
+ export default function Mybutton (){
+ return <button>like</button>
+
+}
