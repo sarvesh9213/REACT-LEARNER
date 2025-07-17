8780592ead6d5e827ff996eeb21f871dbb2fb7b1
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-07-18 02:14:34 +0530

    Learned Conditional Rendering Methods in React
    Explored and implemented three key approaches to conditional rendering in React:
    
    If-Else Statement – Used basic if-else logic inside the component to render elements conditionally.
    
    Ternary Operator – Applied inline conditional rendering using the condition ? true : false format within JSX.
    
    Logical AND (&&) Operator – Used condition && <JSX> to render elements only when the condition is true.
    
    These methods improve the flexibility of UI control and are foundational for building dynamic React components.

--- 18 fragments/learning-fragments/src/App.jsx
+++ 18 fragments/learning-fragments/src/App.jsx
@@ -3,10 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
+  // let foods = [];
   let foods = ["Fruits", "Vegetables", "Grains", "Protein Foods", "Dairy"];
+  let emptymessage = foods.length == 0 ? <h3>i am hungry</h3> : null;
   return (
     <>
       <h1 className="fst-italic">HEALTHY FOODS</h1>
+      {emptymessage}
+      {/* {foods.length == 0 && <h3>i am hungry</h3>} */}
       <ul className="list-group">
         {foods.map((item) => (
           <li key={item} className="list-group-item">
