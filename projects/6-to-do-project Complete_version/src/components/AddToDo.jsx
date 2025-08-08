33119ef2b73d72d4ba6b300d537ee4eac54757e5
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-08-08 23:45:59 +0530

    copied to do project file
    making the to do ui functional with my learnings

---
+++ projects/6-to-do-project Complete_version/src/components/AddToDo.jsx
@@ -0,0 +1,22 @@
+import styles from "./AddToDo.module.css";
+function AddTodo() {
+  return (
+    <div className="container text-center ">
+      <div className={"row kg-row ${styles.inputContainer}"}>
+        <div className="col-6">
+          <input type="text" placeholder="Enter Todo Here" />
+        </div>
+        <div className="col-4">
+          <input type="date" />
+        </div>
+        <div className="col-2">
+          <button type="button" className="btn btn-success kg-button">
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddTodo;
