2468df9e0df8372facb3576f5b92ebbb34958e14
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-08-09 01:11:50 +0530

    my attempt
    i did better than i expected

--- projects/6-to-do-project Complete_version/src/App.jsx
+++ projects/6-to-do-project Complete_version/src/App.jsx
@@ -2,9 +2,10 @@ import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
 import TodoItems from "./components/TodoItems";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const todoItems = [
+  const [todoItems, settodoItems] = useState([
     {
       name: "Buy Milk",
       dueDate: "4/10/2023",
@@ -17,13 +18,23 @@ function App() {
       name: "Like this video",
       dueDate: "right now",
     },
-  ];
-  /* these are basically some examples in which we have created an object in the arrays sarvesh babu */
+  ]); /* these are basically some examples in which we have created an object in the arrays sarvesh babu */
+  const onkeydown = (event) => {
+    if (event.key === "Enter") {
+      const newTodo = {
+        name: event.target.value,
+        dueDate: new Date().toLocaleDateString(),
+      };
+      settodoItems([...todoItems, newTodo]);
+      event.target.value = ""; // Clear the input field after adding
+    }
+    // This function will handle the change in the input field
+  };
 
   return (
     <center className="todo-container">
       <AppName />
-      <AddTodo />
+      <AddTodo onkeydown={onkeydown} />
       <TodoItems todoItems={todoItems}></TodoItems>
       {/* we are passing the todoItems as a prop to the TodoItems component */}
     </center>
--- projects/6-to-do-project Complete_version/src/components/AddToDo.jsx
+++ projects/6-to-do-project Complete_version/src/components/AddToDo.jsx
@@ -1,13 +1,17 @@
 import styles from "./AddToDo.module.css";
-function AddTodo() {
+function AddTodo({ onkeydown, changeDueDate }) {
   return (
     <div className="container text-center ">
       <div className={"row kg-row ${styles.inputContainer}"}>
         <div className="col-6">
-          <input type="text" placeholder="Enter Todo Here" />
+          <input
+            type="text"
+            placeholder="Enter Todo Here"
+            onKeyDown={onkeydown}
+          />
         </div>
         <div className="col-4">
-          <input type="date" />
+          <input type="date" onChange={(e) => setDueDate(e.target.value)} />
         </div>
         <div className="col-2">
           <button type="button" className="btn btn-success kg-button">
