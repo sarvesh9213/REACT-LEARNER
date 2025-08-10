acd9ac0e56c175a5096fcbb9209be7aec73dea1c
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-08-10 21:58:28 +0530

    Update App.jsx
    did revision of the basics

--- LEARNING_BASICS/src/App.jsx
+++ LEARNING_BASICS/src/App.jsx
@@ -1,18 +1,17 @@
-import Mybutton from "./mybutton";  
+import Mybutton from "./mybutton";
 import Hello from "./Hello";
 import Random from "./Random";
-function App(){
-return <div>
-<h1>
-   hello sarvesh
-</h1>
- <Hello></Hello>
- <Random/>
- <Random></Random>
- <Random></Random>
- <Random></Random>
- <Random></Random>
-
-</div>
+function App() {
+  return (
+    <div>
+      <h1>hello sarvesh did revision of the 1 hr intro on react</h1>
+      <Hello></Hello>
+      <Random />
+      <Random></Random>
+      <Random></Random>
+      <Random></Random>
+      <Random></Random>
+    </div>
+  );
 }
-export default App ;
+export default App;
