455504246859cd74f5f3ed4480d2bd49d11c7a07
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-08-12 02:44:24 +0530

    made a card
    i was practicing

---
+++ projects/testing-skills/a card/src/App.jsx
@@ -0,0 +1,14 @@
+import Card from "./Components/Card";
+function App() {
+  return (
+    <>
+      <h1> HEALTH SUPPLIES </h1>
+      <input type="text" placeholder="Search" className="input" />
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+    </>
+  );
+}
+
+export default App;
---
+++ projects/testing-skills/a card/src/Components/Card.jsx
@@ -0,0 +1,14 @@
+import styles from "./Card.module.css";
+const Card = () => {
+  {
+    return (
+      <div className="card">
+        <div>name</div>
+        <div>discription</div>
+        <div>price</div>
+        <button>buy</button>
+      </div>
+    );
+  }
+};
+export default Card;
