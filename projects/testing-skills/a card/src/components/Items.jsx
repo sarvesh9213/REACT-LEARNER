2d6663c207054cab1dba5b715470a1f26f6dae90
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-08-14 19:01:01 +0530

    import
    import

--- projects/testing-skills/a card/src/components/Items.jsx
+++ projects/testing-skills/a card/src/components/Items.jsx
@@ -1,13 +1,16 @@
+import React from "react";
+import styles from "./Card.module.css"; // Reuse the same CSS module
+
 const Items = ({ name, description, price }) => {
   return (
     <div>
-      <div className="product-info">
+      <div className={styles["product-info"]}>
         <div>{name}</div>
         <div>{description}</div>
       </div>
-      <div className="card-bottom">
-        <div className="price">{price}</div>
-        <button className="buy-button">Buy</button>
+      <div className={styles["card-bottom"]}>
+        <div className={styles.price}>{price}</div>
+        <button className={styles["buy-button"]}>Buy</button>
       </div>
     </div>
   );
