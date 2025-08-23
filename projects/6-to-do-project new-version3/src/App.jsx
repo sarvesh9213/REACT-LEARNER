80f0a0b57b7b01d6af9efec067b0e4965fdb797d
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-08-23 17:42:34 +0530

    welcome message added
    added how to use the welcome message  if no task is displayed

--- projects/6-to-do-project new-version3/src/App.jsx
+++ projects/6-to-do-project new-version3/src/App.jsx
@@ -3,22 +3,10 @@ import AddTodo from "./components/AddTodo";
 import TodoItems from "./components/TodoItems";
 import "./App.css";
 import { useState } from "react";
+import Welcome from "./components/Welcome";
 
 function App() {
-  const initialtodoItems = [
-    {
-      name: "Buy Milk",
-      dueDate: "4/10/2023",
-    },
-    {
-      name: "Go to College",
-      dueDate: "4/10/2023",
-    },
-    {
-      name: "Like this video",
-      dueDate: "right now",
-    },
-  ];
+  const initialtodoItems = [];
   const [todoItems, setTodoItems] = useState(initialtodoItems);
   const handleNewitem = (itemName, itemDate) => {
     const newtodoitems = [...todoItems, { name: itemName, dueDate: itemDate }];
@@ -29,6 +17,7 @@ function App() {
     <center className="todo-container">
       <AppName />
       <AddTodo OnNewItem={handleNewitem} />
+      {todoItems.length === 0 && <Welcome></Welcome>}
       <TodoItems todoItems={todoItems}></TodoItems>
       {/* we are passing the todoItems as a prop to the TodoItems component */}
     </center>
---
+++ projects/6-to-do-project new-version3/src/components/Welcome.jsx
@@ -0,0 +1,9 @@
+import styles from "./Welcome.module.css";
+const Welcome = () => {
+  return (
+    <div className={styles.welcome}>
+      <p>PLEASE ENTER YOUR TASK ABOVE </p>
+    </div>
+  );
+};
+export default Welcome;
