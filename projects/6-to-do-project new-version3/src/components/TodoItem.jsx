d10b9e3e7ba66be6d2ee31744a80701e860dfab7
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-09-01 23:54:18 +0530

    Update TodoItem.jsx

--- projects/6-to-do-project new-version3/src/components/TodoItem.jsx
+++ projects/6-to-do-project new-version3/src/components/TodoItem.jsx
@@ -3,7 +3,7 @@ function TodoItem({ todoName, todoDate, onDeleteclick }) {
   return (
     <div className="container">
       <div className="row kg-row">
-        <div className="col-6">{todoName}</div>
+        <div className="col-4">{todoName}</div>
         <div className="col-4">{todoDate}</div>
         <div className="col-2">
           <button
