54a50eada3bc2641f51780a2cca9c57b27c69d22
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-09-08 03:25:50 +0530

    Update App.jsx

--- projects/testing-skills/todoUI/src/App.jsx
+++ projects/testing-skills/todoUI/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <h1>TO DO APP </h1>;
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input type="text" placeholder="enter ur task" />
+        <input type="date" />
+        <button>ADD</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div class="grid text-center">
+        <div class="g-col-4">.buy milk</div>
+        <div class="g-col-4">9/10/25</div>
+        <div class="g-col-2">
+          <button>DELETE</button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div class="grid text-center">
+        <div class="g-col-4">.g-col-4</div>
+        <div class="g-col-4">.g-col-4</div>
+        <div class="g-col-4">.g-col-4</div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
