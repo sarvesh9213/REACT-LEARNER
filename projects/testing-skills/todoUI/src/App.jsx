9c3e3aa76ad70fa6ce5e2337161c756300220b48
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-09-08 03:42:22 +0530

    Update App.jsx

--- projects/testing-skills/todoUI/src/App.jsx
+++ projects/testing-skills/todoUI/src/App.jsx
@@ -4,25 +4,16 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  Name;
   return (
     <>
       <h1>TO DO APP </h1>;
-      <div>
-        <input type="text" placeholder="enter ur task" />
-        <input type="date" />
-        <button>ADD</button>
-      </div>
-      <div class="grid text-center">
-        <div class="g-col-4">.buy milk</div>
-        <div class="g-col-4">9/10/25</div>
-        <div class="g-col-2">
-          <button>DELETE</button>
-        </div>
-      </div>
-      <div class="grid text-center">
-        <div class="g-col-4">.g-col-4</div>
-        <div class="g-col-4">.g-col-4</div>
-        <div class="g-col-4">.g-col-4</div>
+      <div
+        className="grid text-center"
+        style="--bs-columns: 10; --bs-gap: 1rem;"
+      >
+        <div className="g-col-6">.g-col-6</div>
+        <div className="g-col-4">.g-col-4</div>
       </div>
     </>
   );
