dbaa8a2e56a3edec9e5c154f5b23054023038585
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-09-07 23:54:46 +0530

    Update AddToDo.jsx

--- projects/6-to-do-project new-version3/src/components/AddToDo.jsx
+++ projects/6-to-do-project new-version3/src/components/AddToDo.jsx
@@ -28,7 +28,7 @@ function AddTodo({ OnNewItem }) {
           />
         </div>
         <div className="col-2">
-          <input type="date" onChange={handleDateChange} />
+          <input type="text" onChange={handleDateChange} />
         </div>
         <div className="col-2">
           <button
