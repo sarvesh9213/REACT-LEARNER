3a4430a41c9ad6bc49449bbe35289d2ef52bbe08
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-09-20 19:41:26 +0530

    CONTEXT API FOLDER

---
+++ context API/6-to-do-project new-version3/src/components/TodoItem.jsx
@@ -0,0 +1,22 @@
+import { RiDeleteBin6Line } from "react-icons/ri";
+function TodoItem({ todoName, todoDate, onDeleteclick }) {
+  return (
+    <div className="container">
+      <div className="row kg-row">
+        <div className="col-4">{todoName}</div>
+        <div className="col-4">{todoDate}</div>
+        <div className="col-2">
+          <button
+            type="button"
+            className="btn btn-danger kg-button"
+            onClick={() => onDeleteclick(todoName)}
+          >
+            <RiDeleteBin6Line />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TodoItem;
---
+++ context API/6-to-do-project new-version3/src/components/Welcome.jsx
@@ -0,0 +1,9 @@
+import styles from "./Welcome.module.css";
+const Welcome = () => {
+  return (
+    <div className={styles.welcome}>
+      <p>PLEASE ENTER YOUR TASK ABOVE 👋⚛️ </p>
+    </div>
+  );
+};
+export default Welcome;
