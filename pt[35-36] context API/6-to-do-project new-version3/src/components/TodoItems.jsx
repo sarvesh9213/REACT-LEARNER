8378c0a98c81296e12e45a4e5694c1df7d0fd88e
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-09-20 19:34:58 +0530

    copied the latest to do

---
+++ pt[35-36] context API/6-to-do-project new-version3/src/components/TodoItems.jsx
@@ -0,0 +1,19 @@
+import TodoItem from "./TodoItem";
+import styles from "./TodoItems.module.css";
+
+const TodoItems = ({ todoItems, onDeleteclick }) => {
+  return (
+    <div className={styles.itemsContainer}>
+      {todoItems.map((item) => (
+        <TodoItem
+          key={item.name}
+          todoDate={item.dueDate}
+          todoName={item.name}
+          onDeleteclick={onDeleteclick}
+        ></TodoItem>
+      ))}
+    </div>
+  );
+};
+
+export default TodoItems;
