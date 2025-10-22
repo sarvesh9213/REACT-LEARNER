083d2b7598ad41698e1406003af281b6c5b69554
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-10-22 23:33:31 +0530

    Update App.jsx

--- context API/6-to-do-project new-version3/src/App.jsx
+++ context API/6-to-do-project new-version3/src/App.jsx
@@ -33,7 +33,7 @@ function App() {
   };
 
   return (
-    <TodoItemsContext.Provider value={{ todoItems, DeleteItem }}>
+    <TodoItemsContext.Provider value={{ todoItems, addNewItem, DeleteItem }}>
       <center className="todo-container">
         <AppName />
         <AddTodo />
