92d42fd0e4c2852fff193d7907906e99d84342ed
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-10-24 23:32:13 +0530

    Update App.jsx

--- context API/6-to-do-project new-version3/src/App.jsx
+++ context API/6-to-do-project new-version3/src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 import { useReducer, useState } from "react";
 import Welcome from "./components/Welcome";
 import { TodoItemsContext } from "./store/todo-items-store";
-const todoItemsReducer = () => {
+const todoItemsReducer = (action) => {
   // its like usestate but another type of a hook
   return [];
 };
